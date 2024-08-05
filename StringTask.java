3b8649e18c2e99ad6749202489c583248d66259c
Naveen00005 <147023716+Naveen00005@users.noreply.github.com>
2024-08-05 18:10:32 +0530

    Update StringTask.java

--- StringTask.java
+++ StringTask.java
@@ -2,45 +2,36 @@ package stringprograms;
 import java.util.Scanner;
 public class StringTask{
 
-
+public void nullPointer(String nullcheck)throws CustomExceptions {
+        if (nullcheck == null) {
+            throw new CustomExceptions("Input string is null.");
+        }
+}
 
 public int lengthOfString(String slength) throws CustomExceptions {
-        if (slength == null) {
-            throw new CustomExceptions("Input array is null.");
-        }
+        nullPointer(slength);
         int totalLength = slength.length();
-     return totalLength;
+        return totalLength;
 }
 
 public char[] convertItIntoCharacterArray(String character) throws CustomExceptions {
-        if (character == null) {
-            throw new CustomExceptions("Input string is null.");
-        }
+        nullPointer(character);
         return character.toCharArray();
 }
 
-
-public String penultimate(int occurrence, String character) throws CustomExceptions {
-       
-	if (character == null ) {          
-            throw new CustomExceptions("Input string is null.");
-        }
-	int chara = lengthOfString(character);
-	
+public char penultimate(int occurrence, String character) throws CustomExceptions {
+	int chara = lengthOfString(character);	
         if (chara < occurrence) {
             throw new CustomExceptions("Not able to find penultimate: occurrence exceeds string length.");
         }
         int index = chara - occurrence;
-        return "From the given position, it contains: " + character.charAt(index);
+	char letter = character.charAt(index);
+	return letter;
 }
 
 public int numberOfOccurrences(String occurrences,char character)throws CustomExceptions{
-	int count = 0;
-
-	if(occurrences == null) {
-		throw new CustomExceptions("Input string is null.");
-	} 
 	int length = lengthOfString(occurrences);
+	int count = 0;
 	for(int i=0;i<length;i++) {
 		if(occurrences.charAt(i) == character){
 			count++;
@@ -49,12 +40,7 @@ public int numberOfOccurrences(String occurrences,char character)throws CustomEx
 	return count;
 }
 
-
 public int greatestPosition(String greatest,char character) throws CustomExceptions{
-	if(greatest == null) {
-		throw new CustomExceptions("Input string is null.");
-	}
-		
 	int maxPosition = 0;
 	int length = lengthOfString(greatest);
 	for (int i = 0; i < length; i++) {
@@ -66,44 +52,34 @@ public int greatestPosition(String greatest,char character) throws CustomExcepti
 		throw new CustomExceptions("No match found");	
 	} 
 	return maxPosition;	
-	
 }
 
 public String lastFiveCharacters(String character,int position) throws CustomExceptions{
-	if(character == null){
-		throw new CustomExceptions("Input string is null.");
-	}
 	int chara = lengthOfString(character);
 	if(chara< position) {
 		throw new CustomExceptions ("Enter a valid input");
 	}
 
 	String result=character.substring(chara- position,chara);
-	return "The last "+position+" Characters "+ result;
+	return result;
 
 }
 
 public String firstThreeCharacters(String character,int position) throws CustomExceptions {
-if(character == null){
-throw new CustomExceptions("Input string is null.");
-}
 if (lengthOfString(character) >= position) {
 String result = character.substring(0, position);
-return "From the first "+position+" character :"+result;
+return result;
 } else {
 throw new CustomExceptions ( "String is too short to find first three characters");
 }
 }
 
 public String replaceFirstThreeCharacters(String character,String replace)throws CustomExceptions{
-if(character == null){
-throw new CustomExceptions("Input string is null.");
-}
 int number = lengthOfString(replace);
 if(lengthOfString(character)>=number)
 {
 String result=character.substring(0,number);
-return "Replaced the string for the characters :"+character.replace(result,replace);
+return character.replace(result,replace);
 }
 else{
 throw new CustomExceptions ("Enter more character");
@@ -111,73 +87,56 @@ throw new CustomExceptions ("Enter more character");
 }
 
 public String startsWith(String character,String start) throws CustomExceptions{
-if(character == null){
-throw new CustomExceptions("Input string is null.");
-}
-String output;
 int start_number = lengthOfString(start);
 if(lengthOfString(character) >= start_number){
 	String result=character.substring(0,start_number);
 	if(result.equals(start)) {
-		output = "Starts with "+start;
+		return start;
 	}
 	else {
-		output = "Starts with other letters";
+	throw new CustomExceptions("Starts with other letters");
 	}
 }
 else{
 throw new CustomExceptions("Please enter a valid input");
 }
-return output;
 }
 
 public String endsWithLe(String character,String end)throws CustomExceptions{
-if(character == null){
-throw new CustomExceptions("Input string is null.");
-}
-String output;
 int chara = lengthOfString(character);
 int end_num = lengthOfString(end);
 if(chara >= end_num){
 String result=character.substring(chara - end_num,chara);
 if(result.equals(end)) {
-output = "Ends with "+end;
+return end;
 } else {
-output = "Ends with other letters";
+throw new CustomExceptions("Ends with other letters");
 }
 }else{
 throw new CustomExceptions("Please enter a valid input");
 }
-return output;
 }
 
 public String lowercaseToUppercase(String character)throws CustomExceptions{
-if(character == null){
-throw new CustomExceptions("Input string is null.");
-}
+nullPointer(character);
 String result=character.toUpperCase();
-return "Convert all strings to uppercase :" +result;
+return result;
 }
 
 public String uppercaseToLowercase(String character)throws CustomExceptions{
-if(character == null){
-throw new CustomExceptions("Input string is null.");
-}
+nullPointer(character);
 String result=character.toLowerCase();
-return "Convert all strings to lowercase :"+result ;
+return result ;
 }
 
 public String reverseString(String character)throws CustomExceptions{
-if(character == null){
-throw new CustomExceptions("Input string is null.");
-}
 String reverse = "";
 int length = lengthOfString(character);
 for(int i = 0; i < length; i++)
 {
 reverse = character.charAt(i) + reverse;
 }
-return "Reversed string :"+reverse;
+return reverse;
 }
 
 public String acceptMultipleStrings(String[] character,Scanner scanner)throws CustomExceptions{
@@ -189,33 +148,28 @@ for (int i = 0; i <length; i++)
 {
 character[i] = scanner.nextLine();
 } 
-String output = "You have entered: ";
+String output = "";
 for(String str : character)  
 {  
 output = output+"\n"+str;
 }
 return output;
-
 }
-
 
 public String concatenateIndividualStrings(int input,Scanner scanner)throws CustomExceptions{
 if (scanner == null) {
 throw new CustomExceptions("Input string is null.");
-}else{
+}
 String print1="";
 for (int i = 0; i <input; i++)   
 {  
 print1=print1.concat(scanner.nextLine());
 } 
-return "concatenate strings :" +print1;
-}}
-
+return print1;
+}
 
 public String stringToArray(String character)throws CustomExceptions{
-if(character == null){
-throw new CustomExceptions("Input string is null.");
-}
+nullPointer(character);
 String[] words = character.split("\\s+");
 int length = words.length;
 String words_print= "Enclose each string : {";
@@ -249,24 +203,19 @@ print=print+symbol+ scanner.nextLine();
 }
 
 public boolean checkStringEquality(String input, String check)throws CustomExceptions{
-if (input == null || check == null) {
-throw new CustomExceptions("Input string is null.");
-}
+nullPointer(input);
+nullPointer(check);
 return input.equals(check);
 }
 
-public boolean checkStringEqualityIgnoreCase(String character, String IgnoreCase)throws CustomExceptions{
-if (character == null || IgnoreCase == null) {
-throw new CustomExceptions("Input string is null.");
-}
-return character.equalsIgnoreCase(IgnoreCase);
+public boolean checkStringEqualityIgnoreCase(String character, String ignoreCase)throws CustomExceptions{
+nullPointer(character);
+nullPointer(ignoreCase);
+return character.equalsIgnoreCase(ignoreCase);
 }
 
 public String trimString(String character)throws CustomExceptions{
-if (character == null) {
-throw new CustomExceptions("Input string is null.");
-}
+nullPointer(character);
 return character.trim();
-
 }
 }
