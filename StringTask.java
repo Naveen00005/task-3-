5b883d439640fda8ed61fc74df848e11d7accda9
Naveen00005 <147023716+Naveen00005@users.noreply.github.com>
2024-09-13 20:46:56 +0530

    Update StringTask.java

--- StringTask.java
+++ StringTask.java
@@ -1,25 +1,22 @@
-package stringprograms;
+import stringprograms.UtilTask;
+import stringprograms.CustomExceptions;
 public class StringTask{
+UtilTask utilObj = new UtilTask();
 
-public void validateNullString(Object input)throws CustomExceptions {
-        if (input == null) {
-            throw new CustomExceptions("Input cannot be null.");
-        }
-}
 
 public int lengthOfString(String lengthOfTheWord) throws CustomExceptions {
-        validateNullString(lengthOfTheWord);
-        return lengthOfTheWord.length();
+        int length = utilObj.lengthOfTheString(lengthOfTheWord);
+        return length;
 }
 
-public char[] convertItIntoCharacterArray(String character) throws CustomExceptions {
-        validateNullString(character);
-        return character.toCharArray();
+public char[] convertItIntoCharacterArray(String characters) throws CustomExceptions {
+        char[] characterArray = utilObj.convertIntoCharacterArray(characters);
+        return characterArray;
 }
 
 public char stringFromIndex(int occurrence, String character) throws CustomExceptions {
-        validateNullString(character);
-        int length = lengthOfString(character);
+       
+        int length = utilObj.lengthOfTheString(character);
         if (occurrence <= 0 || occurrence > length) {
             throw new CustomExceptions ("The given occurrence position greater than the length");
         }
@@ -27,7 +24,7 @@ public char stringFromIndex(int occurrence, String character) throws CustomExcep
     }
 
 public int numberOfOccurrences(String occurrences,char character)throws CustomExceptions{
-	int length = lengthOfString(occurrences);
+	int length = utilObj.lengthOfTheString(occurrences);
 	int count = 0;
 	for(int i=0; i<length; i++) {
 		if(occurrences.charAt(i) == character){
@@ -39,13 +36,13 @@ public int numberOfOccurrences(String occurrences,char character)throws CustomEx
 
 
 public int greatestPosition(String greatest, char character) throws CustomExceptions {
-        validateNullString(greatest);
+        utilObj.validateNullString(greatest);
         int lastIndex = greatest.lastIndexOf(character);
         return lastIndex; 
     }
 
 public String getLastNCharacters(String character,int position) throws CustomExceptions{
-	int length = lengthOfString(character);
+	int length = utilObj.lengthOfTheString(character);
 	if(length< position) {
 		throw new CustomExceptions ("Position must be within the length of the string.");
 	}
@@ -53,15 +50,15 @@ public String getLastNCharacters(String character,int position) throws CustomExc
 }
 
 public String getFirstNCharacters(String character,int position) throws CustomExceptions {
-if (position < 0 || position > lengthOfString(character)) {
+if (position < 0 || position > utilObj.lengthOfTheString(character)) {
 throw new CustomExceptions ("Position must be within the length of the string.");
 }
 return character.substring(0, position);
 }
 
 public String replaceCharactersFromBeginning(String character,String replace)throws CustomExceptions{
-int number = lengthOfString(replace);
-if(lengthOfString(character) < number) {
+int number = utilObj.lengthOfTheString(replace);
+if(utilObj.lengthOfTheString(character) < number) {
 	throw new CustomExceptions ("Replacement string is longer than the original string.");
 }
 	String result=character.substring(0,number);
@@ -70,29 +67,29 @@ if(lengthOfString(character) < number) {
 }
 
 public boolean startsWith(String character,String start) throws CustomExceptions{
-validateNullString(character);
-validateNullString(start);
+utilObj.validateNullString(character);
+utilObj.validateNullString(start);
 return character.startsWith(start);
 } 
 
 public boolean endsWith(String character,String end)throws CustomExceptions{
-validateNullString(character);
-validateNullString(end);
+utilObj.validateNullString(character);
+utilObj.validateNullString(end);
 return character.endsWith(end);
 }
 
 public String convertToUpperCase(String character)throws CustomExceptions{
-validateNullString(character);
+utilObj.validateNullString(character);
 return character.toUpperCase();
 }
 
 public String convertToLowerCase(String character)throws CustomExceptions{
-validateNullString(character);
+utilObj.validateNullString(character);
 return character.toLowerCase();
 }
 
 public String reverseString(String stringToReverse) throws CustomExceptions {
-	 char[] charArray = convertItIntoCharacterArray(stringToReverse);
+	 char[] charArray = utilObj.convertIntoCharacterArray(stringToReverse);
 	int start = 0;
 	int end = charArray.length - 1;  
         while (start < end) {  
@@ -105,42 +102,42 @@ public String reverseString(String stringToReverse) throws CustomExceptions {
 
 
 public String acceptMultipleStrings(String character)throws CustomExceptions{
-validateNullString(character);
+utilObj.validateNullString(character);
 return character;
 }
 
 public String concatenateIndividualStrings(String input , String concatSymbol)throws CustomExceptions{
-validateNullString(input);
+utilObj.validateNullString(input);
 String[] concatenate = input.split(concatSymbol);
 return String.join("", concatenate);
 }
 
 public String[] stringToArray(String character, String symbol)throws CustomExceptions{
-validateNullString(character);
+utilObj.validateNullString(character);
 String[] words = character.split(symbol);
 return words;
 }
 
 public String mergeStrings(String[] input,String symbol)throws CustomExceptions{
-validateNullString(input);
-validateNullString(symbol);
+utilObj.validateNullString(input);
+utilObj.validateNullString(symbol);
 return String.join(symbol, input);
 }
 
 public boolean checkStringEquality(String input, String check)throws CustomExceptions{
-validateNullString(input);
-validateNullString(check);
+utilObj.validateNullString(input);
+utilObj.validateNullString(check);
 return input.equals(check);
 }
 
 public boolean checkStringEqualityIgnoreCase(String character, String ignoreCase)throws CustomExceptions{
-validateNullString(character);
-validateNullString(ignoreCase);
+utilObj.validateNullString(character);
+utilObj.validateNullString(ignoreCase);
 return character.equalsIgnoreCase(ignoreCase);
 }
 
 public String trimString(String character)throws CustomExceptions{
-validateNullString(character);
+utilObj.validateNullString(character);
 return character.trim();
 }
 }
