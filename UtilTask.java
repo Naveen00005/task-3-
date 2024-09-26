a7c54ad362ef2a3a32f8c5bc579a4099cb3559bb
Naveen00005 <147023716+Naveen00005@users.noreply.github.com>
2024-09-26 20:41:03 +0530

    Update UtilTask.java

--- UtilTask.java
+++ UtilTask.java
@@ -2,24 +2,30 @@ package stringprograms;
 
 public class UtilTask{
 
-public void validateNullString(Object input)throws CustomExceptions {
+public static void validateNullString(Object input)throws CustomExceptions {
         if (input == null) {
             throw new CustomExceptions("Input cannot be null.");
         }
 }
 
-public int lengthOfTheString(String stringLength) throws CustomExceptions {
+public static int lengthOfTheString(String stringLength) throws CustomExceptions {
         validateNullString(stringLength);
         return stringLength.length();
 }
 
-public char[] convertIntoCharacterArray(String character) throws CustomExceptions {
+public static char[] convertIntoCharacterArray(String character) throws CustomExceptions {
         validateNullString(character);
         return character.toCharArray();
 }
 
-public int getLength(StringBuilder sb) {
+public static int getLength(StringBuilder sb) throws CustomExceptions{
 	validateNullString(sb);
         return sb.length();
+}
+
+public static void validateIndices(int initial, int end, int length) throws CustomExceptions {
+        if (initial < 0 || end < 0 || initial > end || end > length) {
+            throw new CustomExceptions("Invalid indices: initial = " + initial + ", end = " + end);
+}
 }
 }
