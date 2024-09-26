5124f9dd5491c3bc1ad5847f845108b589ceb538
Naveen00005 <147023716+Naveen00005@users.noreply.github.com>
2024-09-26 20:40:33 +0530

    Update StringBuilderRunner.java

--- StringBuilderRunner.java
+++ StringBuilderRunner.java
@@ -7,15 +7,15 @@ class StringBuilderRunner {
 public static void main(String args[]){
 Scanner scanner = new Scanner(System.in);
 StringBuilderTask builderTask = new StringBuilderTask();
-UtilTask utilObj = new UtilTask();
+
 
 try{
-StringBuilder sb = new StringBuilder();
-System.out.println("Initial Length in the string builder : " + utilObj.getLength(sb));
+StringBuilder sb = builderTask.createStringBuilder("");
+System.out.println("Initial Length in the string builder : " + UtilTask.getLength(sb));
 System.out.println("Enter a String to print the length :");
 String string = scanner.nextLine();
 sb = builderTask.appendString(sb ,string);
-System.out.println("The length after appending: " +  utilObj.getLength(sb));
+System.out.println("The length after appending: " +  UtilTask.getLength(sb));
 System.out.println("The final string is: " + sb);
 }catch (Exception e) {
 System.out.println(e.getMessage());
@@ -24,8 +24,8 @@ System.out.println(e.getMessage());
 try{
 System.out.println("\nEnter an initial string :");
 String initial = scanner.nextLine();
-StringBuilder sb = new StringBuilder(initial);
-System.out.println("The initial length of the StringBuilder: " + utilObj.getLength(sb));
+StringBuilder sb = builderTask.createStringBuilder(initial);
+System.out.println("The initial length of the StringBuilder: " +UtilTask.getLength(sb));
 System.out.println("Enter the symbol to append : ");
 String sym = scanner.nextLine();
 System.out.println("Enter number of Strings to append : ");
@@ -35,7 +35,7 @@ System.out.print("Enter STRING " + (i + 1) + " to append : " );
 String str = scanner.nextLine();
 sb = builderTask.appendStringWithSymbol(sym,sb, str);
 }
-System.out.println("The length after adding strings: " +utilObj.getLength(sb));
+System.out.println("The length after adding strings: " +UtilTask.getLength(sb));
 System.out.println("The final string: " + sb);
 }catch (Exception e) {
 System.out.println(e.getMessage());scanner.nextLine();
@@ -44,14 +44,14 @@ System.out.println(e.getMessage());scanner.nextLine();
 try{
 System.out.println("\nEnter multiple string separated by symbol :");
 String twoString = scanner.nextLine();
-StringBuilder sb1 = new StringBuilder(twoString);
-System.out.println("The length of the original StringBuilder: " + utilObj.getLength(sb1));
+StringBuilder sb1 =  builderTask.createStringBuilder(twoString);
+System.out.println("The length of the original StringBuilder: " + UtilTask.getLength(sb1));
 System.out.println("Enter a symbol to add a string :");
 String symbol = scanner.nextLine();
 System.out.println("Enter a string to insert between the strings :");
 String insertString = scanner.nextLine();
 StringBuilder finsert = builderTask.insertInStringBuilder(sb1,symbol,insertString);
-System.out.println("The length after insertion : " + utilObj.getLength(finsert));
+System.out.println("The length after insertion : " + UtilTask.getLength(finsert));
 System.out.println("The final string: " +finsert);
 }catch (Exception e) {
 System.out.println(e.getMessage());
@@ -60,12 +60,12 @@ System.out.println(e.getMessage());
 try{
 System.out.println("\nEnter multiple Strings separated by symbol to delete :");
 String twoStrings = scanner.nextLine();
-StringBuilder sbdelete = new StringBuilder(twoStrings);
-System.out.println("The length of the original StringBuilder: " + utilObj.getLength(sbdelete));
+StringBuilder sbdelete = builderTask.createStringBuilder(twoStrings);
+System.out.println("The length of the original StringBuilder: " + UtilTask.getLength(sbdelete));
 System.out.println("Enter a symbol to delete :");
 String symbols = scanner.nextLine();
 StringBuilder delete = builderTask.deleteInitialString(sbdelete,symbols);
-System.out.println("The length after deleted : " + utilObj.getLength(delete));
+System.out.println("The length after deleted : " + UtilTask.getLength(delete));
 System.out.println("The final string: " +delete);
 }catch (Exception e) {
 System.out.println(e.getMessage());
@@ -74,14 +74,14 @@ System.out.println(e.getMessage());
 try{
 System.out.println("\nEnter a line in string builder to replace :");
 String line = scanner.nextLine();
-StringBuilder sb2 = new StringBuilder(line);
-System.out.println("The length of the original StringBuilder: " + utilObj.getLength(sb2));
+StringBuilder sb2 = builderTask.createStringBuilder(line);
+System.out.println("The length of the original StringBuilder: " + UtilTask.getLength(sb2));
 System.out.println("Enter a string to replace :");
 String replace = scanner.nextLine();
 System.out.println("Enter a symbol to replace :");
 String sReplace = scanner.nextLine();
 StringBuilder replaced = builderTask.replaceTheString(sb2,replace,sReplace);
-System.out.println("Length after replacing the symbols " + utilObj.getLength(replaced));
+System.out.println("Length after replacing the symbols " + UtilTask.getLength(replaced));
 System.out.println("Final String: " + replaced);
 }catch (Exception e) {
 System.out.println(e.getMessage());
@@ -90,10 +90,10 @@ System.out.println(e.getMessage());
 try{
 System.out.println("\nEnter a line in string builder to reverse :");
 String reverse  = scanner.nextLine();
-StringBuilder sb3 = new StringBuilder(reverse);
-System.out.println("The length of the original StringBuilder: " + utilObj.getLength(sb3));
+StringBuilder sb3 =  builderTask.createStringBuilder(reverse);
+System.out.println("The length of the original StringBuilder: " + UtilTask.getLength(sb3));
 StringBuilder reversing = builderTask.reverseTheString(sb3);
-System.out.println("Length after replacing the symbols " + utilObj.getLength(reversing ));
+System.out.println("Length after replacing the symbols " + UtilTask.getLength(reversing ));
 System.out.println("Final String: " + reversing);
 }catch (Exception e) {
 System.out.println(e.getMessage());
@@ -102,14 +102,14 @@ System.out.println(e.getMessage());
 try{
 System.out.println("\nEnter the string in Stringbuilder specific delete characters : ");
 String input = scanner.nextLine();
-StringBuilder sb4 = new StringBuilder(input);
-System.out.println("The length of the original StringBuilder: " + utilObj.getLength(sb4));
+StringBuilder sb4 =  builderTask.createStringBuilder(input);
+System.out.println("The length of the original StringBuilder: " + UtilTask.getLength(sb4));
 System.out.println("Enter the starting index number to delete : ");
 int from = scanner.nextInt();scanner.nextLine();
 System.out.println("Enter the ending index number to delete : ");
 int to = scanner.nextInt();scanner.nextLine();
 StringBuilder delete = builderTask.deleteTheString(sb4,from,to);
-System.out.println("Length after deletion: " + utilObj.getLength(delete));
+System.out.println("Length after deletion: " + UtilTask.getLength(delete));
 System.out.println("Final String: " + delete);
 }catch (Exception e) {
 System.out.println(e.getMessage());scanner.nextLine();
@@ -118,8 +118,8 @@ System.out.println(e.getMessage());scanner.nextLine();
 try{
 System.out.println("\nEnter the string in Stringbuilder specific characters to replace : ");
 String inputReplace = scanner.nextLine();
-StringBuilder sb5 = new StringBuilder(inputReplace);
-System.out.println("The length of the original StringBuilder: " + utilObj.getLength(sb5));
+StringBuilder sb5 =  builderTask.createStringBuilder(inputReplace);
+System.out.println("The length of the original StringBuilder: " + UtilTask.getLength(sb5));
 System.out.println("Enter the starting index number to replace : ");
 int fromReplace = scanner.nextInt();scanner.nextLine();
 System.out.println("Enter the ending index number to replace : ");
@@ -127,7 +127,7 @@ int toReplace = scanner.nextInt();scanner.nextLine();
 System.out.println("Enter the string to replace : ");
 String replaceString = scanner.nextLine();
 StringBuilder afterReplace = builderTask.replacingTheString(sb5,fromReplace,toReplace,replaceString);
-System.out.println("Length after replacement: " + utilObj.getLength(afterReplace));
+System.out.println("Length after replacement: " + UtilTask.getLength(afterReplace));
 System.out.println("Final String: " + afterReplace);
 }catch (Exception e) {
 System.out.println(e.getMessage());scanner.nextLine();
@@ -138,7 +138,7 @@ System.out.println("\nEnter the number of strings in Stringbuilder to find the f
 int noOfString = scanner.nextInt();scanner.nextLine();
 System.out.println("Enter the symbol to append inbetween the string : ");
 String symbolsHash = scanner.nextLine();
-StringBuilder sb6 = new StringBuilder();
+StringBuilder sb6 =  builderTask.createStringBuilder();
 for (int i = 0; i < noOfString ; i++) {
 System.out.print("Enter STRING " + (i + 1) + " to append : " );
 String index = scanner.nextLine();
@@ -148,7 +148,7 @@ builderTask.appendString(sb6,index);
 builderTask.appendStringWithSymbol(index, sb6,symbolsHash);
 }
 }
-System.out.println("The length of the original StringBuilder: " + utilObj.getLength(sb6));
+System.out.println("The length of the original StringBuilder: " + UtilTask.getLength(sb6));
 int firstSymbol=builderTask.indexOfFirstSymbol(sb6,symbolsHash);
 System.out.println("First index position "+ symbolsHash +" is : " + firstSymbol);
 }catch (Exception e) {
@@ -160,7 +160,7 @@ System.out.println("\nEnter the number of strings in Stringbuilder to find the l
 int numberOfString = scanner.nextInt();scanner.nextLine();
 System.out.println("Enter the symbol to append inbetween the string : ");
 String symbolsToFind = scanner.nextLine();
-StringBuilder sb7 = new StringBuilder();
+StringBuilder sb7 =  builderTask.createStringBuilder();
 for (int i = 0; i < numberOfString ; i++) {
 System.out.print("Enter STRING " + (i + 1) + " to append : " );
 String index1 = scanner.nextLine();
@@ -170,7 +170,7 @@ builderTask.appendString(sb7,index1);
 builderTask.appendStringWithSymbol(index1, sb7,symbolsToFind);
 }
 }
-System.out.println("The length of the original StringBuilder: " + utilObj.getLength(sb7));
+System.out.println("The length of the original StringBuilder: " + UtilTask.getLength(sb7));
 int lastSymbol=builderTask.indexOfLastSymbol(sb7,symbolsToFind);
 System.out.println("Last index position "+ symbolsToFind +" is : " + lastSymbol);
 }catch (Exception e) {
