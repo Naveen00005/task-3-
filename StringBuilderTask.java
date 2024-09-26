7be87da1691c72827fe02008472ee743790e37c8
Naveen00005 <147023716+Naveen00005@users.noreply.github.com>
2024-09-26 20:39:57 +0530

    Update StringBuilderTask.java

--- StringBuilderTask.java
+++ StringBuilderTask.java
@@ -1,10 +1,14 @@
 package stringprograms;
 public class StringBuilderTask{
-UtilTask utilObj = new UtilTask();
 
+
+public StringBuilder createStringBuilder(String initial){
+StringBuilder sb = new StringBuilder(initial);
+return sb;
+}
 public StringBuilder appendString(StringBuilder sb,String lengthOfTheWord) throws CustomExceptions {
-utilObj.validateNullString(sb);
-utilObj.validateNullString(lengthOfTheWord);
+UtilTask.validateNullString(sb);
+UtilTask.validateNullString(lengthOfTheWord);
 sb.append(lengthOfTheWord);
 return sb;
 }
@@ -16,71 +20,63 @@ return sb;
 }
 
 public StringBuilder insertInStringBuilder(StringBuilder input , String symbols , String insert ) throws CustomExceptions {
-utilObj.validateNullString(input);
-utilObj.validateNullString(symbols);
-utilObj.validateNullString(insert );
-if(input.indexOf(symbols) == -1){
+UtilTask.validateNullString(input);
+UtilTask.validateNullString(symbols);
+UtilTask.validateNullString(insert );
+int index = input.indexOf(symbols);
+if(index == -1){
 throw new CustomExceptions (" invalid  symbol ");
 }
-String[] parts = input.toString().split(symbols);
-int insertPosition =utilObj.lengthOfTheString(parts[0]);
-input.insert(insertPosition, symbols + insert);
+input.insert(index, symbols + insert);
 return input;
 }
 
 public StringBuilder deleteInitialString(StringBuilder input,String symbol) throws CustomExceptions {
-utilObj.validateNullString(input);
-utilObj.validateNullString(symbol);
-if(input.indexOf(symbol) == -1){
+UtilTask.validateNullString(input);
+UtilTask.validateNullString(symbol);
+int index = input.indexOf(symbol);
+if(index  == -1){
 throw new CustomExceptions ("Enter valid  input ");
 }
-String[] part = input.toString().split(symbol);
-int deletePosition =utilObj.lengthOfTheString(part[0]);
-input.delete(0,deletePosition+1);
+input.delete(0,index +1);
 return input;
 }
 
 public StringBuilder replaceTheString(StringBuilder sb , String input,String replace) throws CustomExceptions {
-utilObj.validateNullString(sb);
-utilObj.validateNullString(input);
-utilObj.validateNullString(replace); 
+UtilTask.validateNullString(sb);
+UtilTask.validateNullString(input);
+UtilTask.validateNullString(replace); 
 int index = sb.indexOf(input);
 if(index  == -1){
 throw new CustomExceptions ("Enter existing string to replace ");
 }
-return sb.replace(index, index + utilObj.lengthOfTheString(input), replace);
+return sb.replace(index, index + UtilTask.lengthOfTheString(input), replace);
 }
 
 public StringBuilder reverseTheString(StringBuilder sb) throws CustomExceptions {
-utilObj.validateNullString(sb);
+UtilTask.validateNullString(sb);
 return sb.reverse();
 }
 
 public StringBuilder deleteTheString(StringBuilder sb , int initial , int end) throws CustomExceptions {
-utilObj.validateNullString(sb);
-if (initial < 0 || end < 0 || initial > end || end > utilObj.getLength(sb)) {
-throw new CustomExceptions("Invalid indices: initial = " + initial + ", end = " + end +"\n ......Enter to continue......" );
-}
+UtilTask.validateNullString(sb);
+UtilTask.validateIndices(initial, end, UtilTask.getLength(sb));
 return sb.delete(initial, end+1);
 }
 
 public StringBuilder replacingTheString(StringBuilder sb , int initial , int end ,String replace) throws CustomExceptions {
-utilObj.validateNullString(sb);
-if (initial < 0 || end < 0 || initial > end || end > utilObj.getLength(sb)) {
-throw new CustomExceptions("Invalid indices: initial = " + initial + ", end = " + end+"\n ......Enter to continue......");
-}
+UtilTask.validateNullString(sb);
+UtilTask.validateIndices(initial, end, UtilTask.getLength(sb));
 return sb.replace(initial, end+1,replace);
 }
 
 public int indexOfFirstSymbol(StringBuilder sb , String symbol) throws CustomExceptions {
-utilObj.validateNullString(sb);
+UtilTask.validateNullString(sb);
 return sb.indexOf(symbol);
 }
 
 public int indexOfLastSymbol(StringBuilder sb , String symbol) throws CustomExceptions {
-utilObj.validateNullString(sb);
+UtilTask.validateNullString(sb);
 return sb.lastIndexOf(symbol);
 }
 }
-
-
