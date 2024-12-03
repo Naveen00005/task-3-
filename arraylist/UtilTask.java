cdf63c2ea35bc3ccfd746679a8bfd0137db0d6d3
Naveen00005 <147023716+Naveen00005@users.noreply.github.com>
2024-12-03 12:02:57 +0530

    Update UtilTask.java

--- arraylist/UtilTask.java
+++ arraylist/UtilTask.java
@@ -3,11 +3,11 @@ import java.util.ArrayList;
 
 public class UtilTask{
 
-public int findSize(ArrayList<?> arrayList) {
+public static int findSize(ArrayList<?> arrayList) {
         return arrayList.size();
 }
 
-public ArrayList<String> createArrayList(){
+public static ArrayList<String> createArrayList(){
 ArrayList<String> array = new ArrayList<String>();
 return array;
 }
