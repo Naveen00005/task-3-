065dadd4e6cc2ffe0e7cd73f4f5ab56fc05c0491
Naveen00005 <147023716+Naveen00005@users.noreply.github.com>
2025-01-31 13:42:40 +0530

    Update ArrayListTask.java

--- arraylist/ArrayListTask.java
+++ arraylist/ArrayListTask.java
@@ -2,56 +2,74 @@ package arrayprograms;
 import java.util.Iterator;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Iterator;
 
 public class ArrayListTask {
 
-public <T> List<T> addElement(List<T> list, T element) throws CustomExceptions {
+public static <T> List<T> addElement(List<T> list, T element) throws CustomExceptions {
 UtilTask.validateNullString(list);
 UtilTask.validateNullString(element);
 list.add(element);
 return list;
 }
 
-public int findIndex(List<Object> array, String searchString) throws CustomExceptions {
+public static int findIndex(List<Object> array, String searchString) throws CustomExceptions {
 UtilTask.validateNullString(array );
 UtilTask.validateNullString(searchString);
-return array.indexOf(searchString);
+ int index = array.indexOf(searchString);
+if (index == -1) {
+ throw new CustomExceptions("The string \"" + searchString + "\" is not found in the ArrayList.");
+}
+return index;
 }
 
-public String iterator (Iterator<Object> array) throws CustomExceptions {
-UtilTask.validateNullString(array );
-String it = "";
-while(array.hasNext()) {
-it += array.next()+"\n";
+public static List<Object> iterator (List<Object> array) throws CustomExceptions {
+UtilTask.validateNullString( array );
+List<Object> stringLists =  UtilTask.createArrayList();
+Iterator<Object> it = array.iterator();
+while (it.hasNext()) {
+stringLists.add(it.next()); 
 }
-return it;
+return stringLists;
 }
-public String printUsingForLoop(List<Object> array) throws CustomExceptions {
+
+public static String printUsingForLoop(List<Object> array) throws CustomExceptions {
 UtilTask.validateNullString(array );
 String result = "";
 for (Object str : array) {
-result += str +"\n";
+ result += str;
 }
 return result ;
 }
 
-public Object getStringAtIndex(List<Object> array, int index) throws CustomExceptions {
+public static Object getStringAtIndex(List<Object> array, int index) throws CustomExceptions {
 UtilTask.validateNullString(array );
+if (index < 0 || index >= UtilTask.findSize(array)) {
+throw new CustomExceptions("Invalid index. Index should be between 0 and " + ( UtilTask.findSize(array) - 1));
+}
 return array.get(index);
 }
 
-public int findFirstIndex(List<Object> array, String str)throws CustomExceptions {
+public static int findFirstIndex(List<Object> array, String str)throws CustomExceptions {
 UtilTask.validateNullString(array );
 UtilTask.validateNullString(str); 
-return array.indexOf(str);
+int firstIndex = array.indexOf(str);
+if (firstIndex == -1) {
+ throw new CustomExceptions("The string \"" + str + "\" is not found in the ArrayList.");
 }
-public int findLastIndex(List<Object> array, String str) throws CustomExceptions {
+return firstIndex;
+}
+public static int findLastIndex(List<Object> array, String str) throws CustomExceptions {
 UtilTask.validateNullString(array );
 UtilTask.validateNullString(str);
-return array.lastIndexOf(str);
+int lastIndex = array.lastIndexOf(str);
+if (lastIndex == -1) {
+ throw new CustomExceptions("The string \"" + str + "\" is not found in the ArrayList.");
+}
+return lastIndex;
 }
  
-public List<Object> addStringAtPosition(List<Object> array, String str, int position) throws CustomExceptions {
+public static List<Object> addStringAtPosition(List<Object> array, String str, int position) throws CustomExceptions {
 UtilTask.validateNullString(array );
 UtilTask.validateNullString(str);
 if (position >= 0 || position <= array.size()) {
@@ -60,7 +78,7 @@ if (position >= 0 || position <= array.size()) {
 return array;
 }
 
-public List<Object> addInSecondArrayList(List<Object> array, int initial, int end) throws CustomExceptions{
+public static List<Object> addInSecondArrayList(List<Object> array, int initial, int end) throws CustomExceptions{
 UtilTask.validateNullString(array );
 List<Object> secondArrayList = UtilTask.createArrayList();
 for (int i=initial;i<=end;i++){
@@ -69,7 +87,7 @@ secondArrayList.add(array.get(i));
 return secondArrayList ;
 }
 
-public List<Object> mergeArrayLists(List<Object> first, List<Object> second) throws CustomExceptions {
+public static List<Object> mergeArrayLists(List<Object> first, List<Object> second) throws CustomExceptions {
 UtilTask.validateNullString(first );
 UtilTask.validateNullString(second );
 List<Object> mergedArrayList = new ArrayList<>();
@@ -78,33 +96,33 @@ mergedArrayList.addAll(second);
 return mergedArrayList ;
 }
 
-public List<Object> removeDecimalAtPosition(List<Object> array, int position) throws CustomExceptions {
+public static List<Object> removeDecimalAtPosition(List<Object> array, int position) throws CustomExceptions {
 UtilTask.validateNullString(array );
 array.remove(position);
 return array;
 }
 
-public List<Object> removeCommonElements(List<Object> first, List<Object> second) throws CustomExceptions {
+public static List<Object> removeCommonElements(List<Object> first, List<Object> second) throws CustomExceptions {
 UtilTask.validateNullString(first );
 UtilTask.validateNullString(second );
 first.removeAll(second);
 return first;
 }
 
-public List<Object>retainCommonElements(List<Object> first, List<Object> second) throws CustomExceptions  {
+public static List<Object>retainCommonElements(List<Object> first, List<Object> second) throws CustomExceptions  {
 UtilTask.validateNullString(first );
 UtilTask.validateNullString(second );
 first.retainAll(second);
 return first;
 }
 
-public List<Object> removeAllLongValues(List<Object> array) throws CustomExceptions {
+public static List<Object> removeAllLongValues(List<Object> array) throws CustomExceptions {
 UtilTask.validateNullString(array );
 array.clear();
 return array;
 }
 
-public boolean checkStringPresence(List<Object> array, String str) throws CustomExceptions {
+public static boolean checkStringPresence(List<Object> array, String str) throws CustomExceptions {
 UtilTask.validateNullString(array );
 UtilTask.validateNullString(str);
 return array.contains(str);
