44493fcf0e8339acc978d2f6d5d81e73f933211f
Naveen00005 <147023716+Naveen00005@users.noreply.github.com>
2025-02-12 08:55:58 +0530

    Update HashMapTask.java

--- hashmap/HashMapTask.java
+++ hashmap/HashMapTask.java
@@ -1,9 +1,8 @@
 package hashmapprograms;
 import java.util.Map;
-import java.util.HashMap;
 public class HashMapTask {
 
-public static <K, V> Map<K, V> putMapElement(Map<K, V> map, K key, V value) throws CustomExceptions {
+public <K, V> Map<K, V> putMapElement(Map<K, V> map, K key, V value) throws CustomExceptions {
 UtilTask.validateNullString(map);
 UtilTask.validateNullString(key);
 UtilTask.validateNullString(value);
@@ -11,19 +10,19 @@ map.put(key, value);
 return map;
 }
 
-public static boolean checkKeyExists(Map<String, String> map, String key)  throws CustomExceptions {
+public boolean checkKeyExists(Map<String, String> map, String key)  throws CustomExceptions {
 UtilTask.validateNullString(map);
 UtilTask.validateNullString(key);
 return map.containsKey(key);
 }
 
-public static boolean checkValueExists(Map<String, String> map, String value)   throws CustomExceptions{
+public boolean checkValueExists(Map<String, String> map, String value)   throws CustomExceptions{
 UtilTask.validateNullString(map);
 UtilTask.validateNullString(value);
 return map.containsValue(value);
 }
 
-public static String getValueByKey(Map<String, String> map, String key)  throws CustomExceptions {
+public String getValueByKey(Map<String, String> map, String key)  throws CustomExceptions {
 UtilTask.validateNullString(map);
 UtilTask.validateNullString(key);
 if (checkKeyExists(map, key)) {
@@ -33,14 +32,14 @@ return "The value for the key \"" + key + "\" is: " + map.get(key);
 }
 }
 
-public static String getDefaultValue (Map<String, String> map, String key, String defaultValue) throws CustomExceptions {
+public String getDefaultValue (Map<String, String> map, String key, String defaultValue) throws CustomExceptions {
 UtilTask.validateNullString(map);
 UtilTask.validateNullString(key);
 UtilTask.validateNullString(defaultValue);
 return map.getOrDefault(key, defaultValue);
 }
 
-public static String removeKey(Map<String, String> map, String key) throws CustomExceptions {
+public String removeKey(Map<String, String> map, String key) throws CustomExceptions {
 if (checkKeyExists(map,key)) {
 map.remove(key);
 return "Key \"" + key + "\" removed successfully." ;
@@ -49,7 +48,7 @@ return "Key \"" + key + "\" removed successfully." ;
 }
 }
 
-public static Map<String, String> removeKeyIfValueMatches(Map<String, String> map, String key, String value) throws CustomExceptions {
+public Map<String, String> removeKeyIfValueMatches(Map<String, String> map, String key, String value) throws CustomExceptions {
 UtilTask.validateNullString(map);
 UtilTask.validateNullString(key);
 UtilTask.validateNullString(value); 
@@ -57,7 +56,7 @@ map.remove(key, value);
 return map;
 }
 
-public static String replaceValue(Map<String, String> map, String key, String newValue) throws CustomExceptions {
+public String replaceValue(Map<String, String> map, String key, String newValue) throws CustomExceptions {
 UtilTask.validateNullString(newValue);
 if (checkKeyExists(map,key)) {
 map.replace(key, newValue);
@@ -67,7 +66,7 @@ throw new CustomExceptions ("Key \"" + key + "\" does not exist in the HashMap."
 }
 }
 
-public static Map<String, String> replaceValueIfMatch(Map<String, String> map, String key, String oldValue, String newValue) throws CustomExceptions {
+public Map<String, String> replaceValueIfMatch(Map<String, String> map, String key, String oldValue, String newValue) throws CustomExceptions {
 UtilTask.validateNullString(oldValue);
 UtilTask.validateNullString(newValue);
 if (checkKeyExists(map,key) && map.get(key).equals(oldValue)) {
@@ -76,24 +75,14 @@ if (checkKeyExists(map,key) && map.get(key).equals(oldValue)) {
 return map;
 }
 
-public static Map<String, String> transferMap(Map<String, String> map,Map<String, String> map2)  throws CustomExceptions  {
+public Map<String, String> transferMap(Map<String, String> map,Map<String, String> map2)  throws CustomExceptions  {
 UtilTask.validateNullString(map);
 map.putAll(map2);
 return map;
 }
 
 
-public static String iterateAndCollect(Map<String, String> map)  throws CustomExceptions  {
-UtilTask.validateNullString(map);
-String val ="";
-for (Map.Entry<String, String> entry : map.entrySet()) {
-  val += "Key: " + entry.getKey() + ", Value: " + entry.getValue()+"\n";
-}
-return val;
-}
-
-
-public static Map<String, String>  removeAll(Map<String, String> map)  throws CustomExceptions  {
+public Map<String, String>  removeAll(Map<String, String> map)  throws CustomExceptions  {
 UtilTask.validateNullString(map);
 map.clear();
 return map;
